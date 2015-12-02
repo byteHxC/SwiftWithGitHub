e91c58104fbe86abb2762885bae8f1c94f3f6fc4
Byter <Byter@macbook-pro-de-byter.local>
2015-12-01 20:07:15 -0600

    Primer cambio en swift

--- FirstProjectWithGitHub/ViewController.swift
+++ FirstProjectWithGitHub/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Cambios en swift way :3")
     }
 
     override func didReceiveMemoryWarning() {
